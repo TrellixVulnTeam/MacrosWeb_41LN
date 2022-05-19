c7f54e91c02741a329ed4387bc089953beb419d3
maasprilladreamcode <miguel.asprilla@dreamcodesoft.com>
2022-05-19 02:55:20 -0500

    actualizacion base de datos y auth

--- src/main/java/co/com/reportstools/DBConfiguration.java
+++ src/main/java/co/com/reportstools/DBConfiguration.java
@@ -22,6 +22,12 @@ public class DBConfiguration {
 		dataSource.setUrl("jdbc:sqlserver://db-cbol-apps-prod.database.windows.net;database=MacrosWebApp;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;");
 		dataSource.setUsername("macros_web_app");
 		dataSource.setPassword("DUQa3Rc#!0@v");
+		
+//		dataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+//		dataSource.setUrl("jdbc:sqlserver://dwh.cbolivar.com:13144;databaseName=MacrosWebApp");
+//		dataSource.setUsername("macroswebapp");
+//		dataSource.setPassword("M4cr0sw3b4pp*2022");
+		
 		return dataSource;
 	}
 
